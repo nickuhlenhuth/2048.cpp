92815fed6caad96370fdd8d031366ea463cba7ed
Nick Uhlenhuth <nicku@mit.edu>
2021-06-08 18:51:56 +0000

    added issue

--- src/2048.cpp
+++ src/2048.cpp
@@ -2,6 +2,7 @@
 #include "menu.hpp"
 #include <sstream>
 #include <iostream>
+#include <cstring>
 
 
 #ifdef WIN32
@@ -28,5 +29,8 @@ int main() {
 
     #endif
   Menu::startMenu();
+  char a[4];
+strcpy(a,"a string longer than 4 characters"); // write past end of buffer (buffer overflow)
+printf("%s\n",a[6]); // read past end of buffer (also not a good idea)
   return 0;
 }
