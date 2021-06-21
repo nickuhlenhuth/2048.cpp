b0102ea6949eb4d92db3c7ce589f9be1d4d51c50
Nick Uhlenhuth <nicku@mit.edu>
2021-06-21 21:12:38 +0000

    test alert

--- src/2048.cpp
+++ src/2048.cpp
@@ -30,7 +30,7 @@ int main() {
     #endif
   Menu::startMenu();
   char a[4];
-strcpy(a,"a string longer than 4 characters"); // write past end of buffer (buffer overflow)
-printf("%s\n",a[6]); // read past end of buffer (also not a good idea)
+  strcpy(a,"test");
+  printf("%s\n",a[6]);
   return 0;
 }
